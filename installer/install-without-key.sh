76063a124b7dcbb36e919b2788aa72d11e1e1b33
diego <145928215+vpsvip7@users.noreply.github.com>
2024-09-03 13:00:55 -0300

    Actualizar install-without-key.sh

--- installer/install-without-key.sh
+++ installer/install-without-key.sh
@@ -4,6 +4,7 @@ rm -rf /etc/localtime &>/dev/null
 ln -s /usr/share/zoneinfo/America/Argentina /etc/localtime &>/dev/null
 
 apt install net-tools -y &>/dev/null
+apt install curl -y &>/dev/null
 myip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1)
 myint=$(ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}')
 rm -rf /etc/localtime &>/dev/null
